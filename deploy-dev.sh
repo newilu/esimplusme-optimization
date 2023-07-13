22e608c2c42f44eb2dff6f82f834da4f83009e3b
Maksim Petrov <m.piatrou@setplex.com>
2023-07-13 20:31:37 +0000

    test pipeline

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,3 +1,4 @@
+echo DOCKERHUB_PASSWORD_VALUE | docker login --username DOCKERHUB_USER_VALUE --password-stdin
 docker pull appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
 docker rm -f pipeline-esim-dev
 docker run -d --restart=always --name pipeline-esim-dev -p 127.0.0.1:6000:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
