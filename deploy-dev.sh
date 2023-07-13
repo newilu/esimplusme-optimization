6434786e62900d8b74d02f2c6907c233abe484d8
Maksim Petrov <m.piatrou@setplex.com>
2023-07-13 20:07:37 +0000

    fix pipeline deploy

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,5 +1,11 @@
-echo DOCKERHUB_PASSWORD_VALUE | docker login --username DOCKERHUB_USER_VALUE --password-stdin
 docker pull appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
 docker rm -f pipeline-esim-dev
-docker run -d --restart-always --name pipeline-esim-dev -p 127.0.0.1:6000:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
-echo "container pipeline-esim-dev started"
+docker run -d --restart=always --name pipeline-esim-dev -p 127.0.0.1:6000:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
+if [[ $? -ne 0 ]]; then
+  echo "deploy FAILED"
+  docker logout
+  exit 1;
+else
+  echo "deploy SUCCESS"  
+fi
+docker logout
