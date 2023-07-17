5b9ac55884f4de1b884c834299d18c12c7d5e5e4
Maksim Petrov <m.piatrou@setplex.com>
2023-07-17 08:40:22 +0000

    change containers name in scripts

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,6 @@
 docker pull appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
-docker rm -f pipeline-esim-prod
-docker run -d --restart=always --name pipeline-esim-prod -p 127.0.0.1:6001:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
+docker rm -f esimplusme-prod
+docker run -d --restart=always --name esimplusme-prod -p 127.0.0.1:6001:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
 if [[ $? -ne 0 ]]; then
   echo "deploy FAILED"
   exit 1;
