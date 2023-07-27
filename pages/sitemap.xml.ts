b0a094bd1c172b4fadd6d39b8d23719a01d59913
vlad karatai <vlad.karatai@mobyrix.com>
2023-07-27 11:04:55 +0300

    fix(seo): update sitemap

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,4 +1,6 @@
 import { NextApiResponse } from "next";
+import { COUNTRY_LIST } from "@/shared/constants";
+import { formatStringToKebabCase } from "@/shared/lib";
 
 function SiteMap() {
   return null;
@@ -9,8 +11,28 @@ export async function getServerSideProps({ res }: { res: NextApiResponse }) {
     "https://admin-blog.esimplus.me/api/sitemap"
   ).then((response) => response.text());
 
+  const buildUrlElement = (url: string) =>
+    `<url>
+        <loc>https://esimplus.me/${url}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1</priority>
+    </url>`;
+
+  const updatedSitemap = sitemap
+    .replace("</urlset>", "")
+    .concat(buildUrlElement("virtual-phone-number/pricing"))
+    .concat(buildUrlElement("virtual-phone-number/united-states/mobile"))
+    .concat(
+      COUNTRY_LIST.map((country) =>
+        buildUrlElement(
+          `virtual-phone-number/${formatStringToKebabCase(country.name)}`
+        )
+      ).join("")
+    )
+    .concat("</urlset>");
+
   res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
+  res.write(updatedSitemap);
   res.end();
 
   return {
