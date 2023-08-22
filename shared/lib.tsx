787f07bab8187337d8f4281472150744b26f9535
vlad karatai <vlad.karatai@mobyrix.com>
2023-08-22 23:21:32 +0800

    fix: generate fake numbers even if backend responds but with empy array

--- shared/lib.tsx
+++ shared/lib.tsx
@@ -127,9 +127,7 @@ function setCookie(name: string, value: string, days: number) {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = `; expires=${date.toUTCString()}`;
   }
-  document.cookie = `${name}=${
-    value || ""
-  }${expires}; domain=.esimplus.me; Path=/;`;
+  document.cookie = `${name}=${value || ""}${expires};  Path=/;`;
 }
 
 function getCookie(name: string): string | null {
