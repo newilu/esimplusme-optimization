8727ddb4760e2d9850de8a174617a3704a527ef8
vlad karatai <vlad.karatai@mobyrix.com>
2023-09-04 15:32:33 +0300

    fix: caching

--- pages/api/warmup-cache.ts
+++ pages/api/warmup-cache.ts
@@ -1,22 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import api from "@/api";
-import states from "@/shared/assets/us-state-info-by-iso.json";
-import { SECOND_PHONE_SUPPORTED_COUNTRIES } from "@/shared/constants";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import api from '@/api';
+import states from '@/shared/assets/us-state-info-by-iso.json';
+import { SECOND_PHONE_SUPPORTED_COUNTRIES } from '@/shared/constants';
+import { Cacheable } from '@/lib/redis';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await Promise.all([
-    api.secondPhone.listSecondPhoneCountries(),
-    ...Object.keys(states).map((state) =>
-      api.secondPhone.getAvailableNumbersByStateISO(state)
-    ),
-    ...SECOND_PHONE_SUPPORTED_COUNTRIES.map((country) =>
-      api.secondPhone.getPhonesByCountry(country)
-    ),
-    api.profiles.listRegions(),
-    api.profiles.listCountries(),
+    Cacheable(api.secondPhone.listSecondPhoneCountries)(),
+    ...Object.keys(states).map((state) => Cacheable(api.secondPhone.getAvailableNumbersByStateISO)(state)),
+    ...SECOND_PHONE_SUPPORTED_COUNTRIES.map((country) => Cacheable(api.secondPhone.getPhonesByCountry)(country)),
+    Cacheable(api.profiles.listRegions)(),
+    Cacheable(api.profiles.listCountries)(),
   ]);
 
   res.status(200).json({});
