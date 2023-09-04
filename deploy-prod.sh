445750cf75d1890367d4c84309219c35615e6d23
Maksim Petrov <m.piatrou@setplex.com>
2023-09-04 08:15:09 +0000

    add replicas prod

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,8 @@
 docker pull appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
 docker rm -f esimplusme-prod
-docker run -d --restart=always --name esimplusme-prod -p 127.0.0.1:6001:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
+docker run -d --restart=always --name esimplusme-prod-1 -p 127.0.0.1:7001:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
+docker run -d --restart=always --name esimplusme-prod-2 -p 127.0.0.1:7002:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
+docker run -d --restart=always --name esimplusme-prod-3 -p 127.0.0.1:7003:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
 if [[ $? -ne 0 ]]; then
   echo "deploy FAILED"
   exit 1;
