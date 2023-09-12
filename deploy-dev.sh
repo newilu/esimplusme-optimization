e46a08428efed84431d183b333415f5979c8df29
m.piatrou <piatroumaxim@gmail.com>
2023-09-12 16:27:02 +0300

    add build redis for dev pipeline

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,6 @@
 docker pull appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
 docker rm -f esimplusme-dev
-docker run -d --restart=always --name esimplusme-dev -p 127.0.0.1:6000:3000 appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
+docker run -d --restart=always --name esimplusme-dev -p 127.0.0.1:6000:3000 -e BUILD_MODE=runtime appvillisdocker/landings:IMAGE_NAME_VALUE-VERSION_VALUE
 if [[ $? -ne 0 ]]; then
   echo "deploy FAILED"
   exit 1;
